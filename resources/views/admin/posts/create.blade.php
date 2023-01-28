6beee501818e2128526923b9ec35f13f5cffb3c2
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-28 19:35:58 +0100

    fix - errors were not shown

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,65 +7,75 @@
         {{-- TITLE  --}}
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
             <div class="invalid-feedback">
-                <ul>
-                    @foreach ($errors->get('title') as $errorMess)
-                        <li>{{ $errorMess }}</li>
-                    @endforeach
-                </ul>
+                @error('title')
+                    <ul>
+                        @foreach ($errors->get('title') as $errorMess)
+                            <li>{{ $errorMess }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
             </div>
         </div>
 
         {{-- SLUG  --}}
         <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-            <input type="text" class="form-control" id="slug" name="slug" value="{{ old('slug') }}">
+            <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" value="{{ old('slug') }}">
             <div class="invalid-feedback">
-                <ul>
-                    @foreach ($errors->get('slug') as $errorMess)
-                        <li>{{ $errorMess }}</li>
-                    @endforeach
-                </ul>
+                @error('slug')
+                    <ul>
+                        @foreach ($errors->get('slug') as $errorMess)
+                            <li>{{ $errorMess }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
             </div>
         </div>
 
         {{-- URL IMAGE  --}}
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="url" class="form-control" id="image" name="image" value="{{ old('image') }}">
+            <input type="url" class="form-control @error('image') is-invalid @enderror" id="image" name="image" value="{{ old('image') }}">
             <div class="invalid-feedback">
-                <ul>
-                    @foreach ($errors->get('image') as $errorMess)
-                        <li>{{ $errorMess }}</li>
-                    @endforeach
-                </ul>
+                @error('image')
+                    <ul>
+                        @foreach ($errors->get('image') as $errorMess)
+                            <li>{{ $errorMess }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
             </div>
         </div>
 
         {{-- CONTENUTO POST --}}
         <div class="mb-3">
             <label for="excerpt" class="form-label">Inserisci l'anteprima del post</label>
-            <textarea class="form-control" id="excerpt" name="excerpt">{{ old('excerpt') }}</textarea>
+            <textarea class="form-control @error('excerpt') is-invalid @enderror" id="excerpt" name="excerpt">{{ old('excerpt') }}</textarea>
             <div class="invalid-feedback">
-                <ul>
-                    @foreach ($errors->get('excerpt') as $errorMess)
-                        <li>{{ $errorMess }}</li>
-                    @endforeach
-                </ul>
+                @error('excerpt')
+                    <ul>
+                        @foreach ($errors->get('excerpt') as $errorMess)
+                            <li>{{ $errorMess }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
             </div>
         </div>
 
         {{-- ANTEPRIMA TESTO --}}
         <div class="mb-3">
             <label for="content" class="form-label">Inserisci il contenuto del post</label>
-            <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
+            <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content">{{ old('content') }}</textarea>
             <div class="invalid-feedback">
-                <ul>
-                    @foreach ($errors->get('content') as $errorMess)
-                        <li>{{ $errorMess }}</li>
-                    @endforeach
-                </ul>
+                @error('content')
+                    <ul>
+                        @foreach ($errors->get('content') as $errorMess)
+                            <li>{{ $errorMess }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
             </div>
         </div>
 
